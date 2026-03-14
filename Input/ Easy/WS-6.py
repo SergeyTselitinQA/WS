bad3fa99740382c3daed0c22c3ae9511db4ccd5e
SergeyTselitinQA <sergeytselitinqa@gmail.com>
2026-03-14 23:22:59 +0300

    WS-6 готов

--- Input/ Easy/WS-6.py
+++ Input/ Easy/WS-6.py
@@ -3,6 +3,9 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--window-size=1920,1080")
@@ -12,4 +15,16 @@ driver = webdriver.Chrome(service=service, options=chrome_options)
 
 driver.get("https://aqa-proka4.org/sandbox/web")
 
+driver.find_element("xpath", "//*[@id='username']").send_keys("Ivan")
+driver.find_element("xpath", "//*[@id='email']").send_keys("test@gmail.com")
+driver.find_element("xpath", "//*[@id='password']").send_keys("Qwerty11!")
+DROPDOWN_ELEMENT = ("xpath", "//*[@id='country']")
+DROPDOWN = Select(driver.find_element(*DROPDOWN_ELEMENT))
+DROPDOWN.select_by_value("ru")
+driver.find_element("xpath", "//*[@id='submitBtn']").click()
+
+pwd = WebDriverWait(driver, 10).until(EC.presence_of_element_located(("xpath", "//*[@id='terms']")))
+val_msg = driver.execute_script("return arguments[0] ? arguments[0].validationMessage : null;", pwd)
+assert val_msg == 'Чтобы продолжить, установите этот флажок.', 'Ошибка в регистации пользователя'
+print("validationMessage:", val_msg)
 time.sleep(3)

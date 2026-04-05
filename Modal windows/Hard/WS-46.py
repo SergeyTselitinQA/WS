ba04ae6cb854179cf0565083df1eb1628077db08
SergeyTselitinQA <sergeytselitinqa@gmail.com>
2026-04-05 18:48:10 +0300

    Написал WS-46 and WS-47

---
+++ Modal windows/Hard/WS-46.py
@@ -0,0 +1,18 @@
+from  selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--window-size=1920,1080")
+
+service = Service(executable_path=ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service, options=chrome_options)
+
+driver.get("https://aqa-proka4.org/sandbox/web")
+
+driver.find_element("xpath", "//*[@id='errorModalBtn']").click()
+modal_total = driver.find_element("xpath", "//*[@id='dynamicModalContainer']")
+assert "Ошибка!" in modal_total.text
+
+modal = driver.find_element("xpath", "(//*[@id='dynamicModalContainer']//div[contains(@class,'border-red-500')])[1]")
+assert "border-red-500" in modal.get_attribute("class")

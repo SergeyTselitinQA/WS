6b0a5af904f0727e78c0d47d6b9a972b6aac5a85
SergeyTselitinQA <sergeytselitinqa@gmail.com>
2026-04-08 23:23:58 +0300

    Fix WS-52

--- Drag & Drop/Medium2/WS-52.py
+++ Drag & Drop/Medium2/WS-52.py
@@ -1,5 +1,3 @@
-import time
-
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
@@ -12,13 +10,13 @@ chrome_options.add_argument("--window-size=1920,1080")
 service = Service(executable_path=ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
-INT_LIST = ['1', '5', '2', '3', '4']
+INT_LIST = ['5', '1', '2', '3', '4']
 
 driver.get("https://aqa-proka4.org/sandbox/web")
 item1 = driver.find_element("xpath", "//*[contains(@data-position,'1')]")
 item5 = driver.find_element("xpath", "//*[contains(@data-position,'5')]")
 
-ActionChains(driver).drag_and_drop(item5, item1).perform()
+ActionChains(driver).click_and_hold(item5).move_to_element_with_offset(item1, 0, -10).release().perform()
 
 items = driver.find_elements("xpath", "//*[contains(@class,'sortable-item')]")
 positions = [item.get_attribute("data-position") for item in items]

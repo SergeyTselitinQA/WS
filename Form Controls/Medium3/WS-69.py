660747ea9969fc302775a5c5084e192b57f8c21e
SergeyTselitinQA <sergeytselitinqa@gmail.com>
2026-04-22 00:28:25 +0300

    Написал на тоггл

--- Form Controls/Medium3/WS-69.py
+++ Form Controls/Medium3/WS-69.py
@@ -12,16 +12,28 @@ driver = webdriver.Chrome(service=service, options=chrome_options)
 
 driver.get("https://aqa-proka4.org/sandbox/web")
 
-email_toggle = driver.find_element("xpath", "//*[@id='emailToggle']")
-if not email_toggle.is_selected():
+email_toggle = driver.find_element("xpath", "(//*[contains(@class,'relative ')])[1]")
+id_email = driver.find_element("xpath", "//*[@id='emailToggle']")
+if not id_email.is_selected():
     email_toggle.click()
 
-push_toggle = driver.find_element("xpath", "//*[@id='pushToggle']")
-if not push_toggle.is_selected():
+push_toggle = driver.find_element("xpath", "(//*[contains(@class,'relative ')])[2]")
+id_push = driver.find_element("xpath", "//*[@id='pushToggle']")
+if not id_push.is_selected():
     push_toggle.click()
 
-dark_toggle = driver.find_element("xpath", "//*[@id='darkModeToggle']")
-if not dark_toggle.is_selected():
+dark_toggle = driver.find_element("xpath", "(//*[contains(@class,'relative ')])[3]")
+id_dark = driver.find_element("xpath", "//*[@id='darkModeToggle']")
+if not id_dark.is_selected():
     dark_toggle.click()
+
+
+
+assert id_email.is_selected() == True
+assert id_push.is_selected() == True
+assert id_dark.is_selected() == True
+
+# color_picker = driver.find_element("xpath", "colorPicker")
+# driver.execute_script("arguments[0].value = '#FF0000'", color_picker)
 
 time.sleep(5)

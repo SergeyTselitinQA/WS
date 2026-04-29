a0fb2cad4f3c1876f9fd68e6906178c7479b4c08
SergeyTselitinQA <sergeytselitinqa@gmail.com>
2026-04-29 23:20:04 +0300

    Написал WS-75 вторая кнопка + меню

--- Advanced/Hard2/WS-75.py
+++ Advanced/Hard2/WS-75.py
@@ -1,5 +1,4 @@
 import time
-from ctypes.macholib.framework import framework_info
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -18,6 +17,8 @@ driver = webdriver.Chrome(service=service, options=chrome_options)
 wait = WebDriverWait(driver, 10)
 
 TOOLTIP = "Это всплывающая подсказка!"
+TOOLTIP_TWO = "Я снизу! 👇"
+MENU = "Главная\nПрофиль\nНастройки\nВыход"
 
 driver.get("https://aqa-proka4.org/sandbox/web")
 
@@ -27,5 +28,17 @@ ActionChains(driver).move_to_element(button_one).perform()
 tooltip = driver.find_element("xpath", "//*[contains(@class,'opacity-100')]").text
 assert tooltip == TOOLTIP
 
+button_two = driver.find_element("xpath", "//*[@id='tooltipBtn2']")
+ActionChains(driver).move_to_element(button_two).perform()
+
+tooltip_btn = driver.find_element("xpath", "//*[@id='tooltip2' and contains(@class, 'opacity-100')]").text
+assert TOOLTIP_TWO == tooltip_btn
+
+menu = driver.find_element("xpath", "//*[@id='hoverMenu']")
+ActionChains(driver).move_to_element(menu).perform()
+
+el = wait.until(EC.visibility_of_element_located(("xpath", "//*[@id='hoverDropdown' and contains(@class,'opacity-100')]")))
+tooltip_menu = el.text
+assert MENU == tooltip_menu
 
 time.sleep(3)

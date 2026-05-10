13df6e53c56bef127c0a6e39b19028934b50d1e7
SergeyTselitinQA <sergeytselitinqa@gmail.com>
2026-05-10 13:23:28 +0300

    Написал WS-80

---
+++ UI Components/WS-80.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--window-size=1920,1080")
+
+service = Service(executable_path=ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service, options=chrome_options)
+
+driver.get("https://aqa-proka4.org/sandbox/web")
+
+driver.find_element("xpath", "//*[@id='tab-javascript']").click()
+active_tab = driver.find_element("xpath", "//*[contains(@class,'tab-button') and contains(@class,'bg-blue-600')]")
+assert "tab-javascript" in active_tab.get_attribute("id")
+
+hidden = driver.find_element("xpath", "//*[@id='content-python']")
+assert "hidden" in hidden.get_attribute("class")

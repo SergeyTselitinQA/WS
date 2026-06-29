3882a82c4b22b30a773a792d4279f5baeee4231c
SergeyTselitinQA <sergeytselitinqa@gmail.com>
2026-06-29 19:41:34 +0300

    Написал срез

---
+++ python/str.py
@@ -0,0 +1,19 @@
+str = "Python"
+
+print(str[3])
+
+text = "Python"
+
+
+# print(text[1:4])   # "yth"  — с индекса 1 до 4 (не включая 4)
+# print(text[:3])    # "Pyt"  — с начала до индекса 3
+# print(text[3:])    # "hon"  — с индекса 3 до конца
+# print(text[:])     # "Python" — вся строка целиком
+# print(text[::-1])  # "nohtyP" — вся строка в обратном порядке
+# print(text[::2])   # "Pto"  — каждый второй символ
+print(text[2:5])
+print(text[:2])
+print(text[2:])
+print(text[:])
+print(text[::-1])
+print(text[::2])
